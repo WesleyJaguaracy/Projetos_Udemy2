959a193a6227f93d5aac2a0ce65f517888a650ae
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-12-09 13:24:33 -0300

    Exercicio33

---
+++ Aula33Exercicio01/Exercicio01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Projetos_Udemy2
+{
+    class Exercicio01
+    {
+        static void Main(string[] args)
+        {
+
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
