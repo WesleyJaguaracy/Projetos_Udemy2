4a92b71018b21600a2e7fb706c507e650ad55c27
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-12-09 13:10:41 -0300

    Add projetos e exercicios

---
+++ Aula29ExercicioProposto01/Exercicio01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Projetos_Udemy2
+{
+    class Exercicio01
+    {
+        static void Main(string[] args)
+        {
+
+
+            int negativo = int.Parse(Console.ReadLine());
+
+            if (negativo < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+        }
+    }
+}
---
+++ Aula29ExercicioProposto02/Exercicio02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cond02
+{
+    class Exercicio02
+    {
+        static void Main(string[] args)
+        {
+
+            int negativo = int.Parse(Console.ReadLine());
+
+            if (negativo % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
